0e715531d9588cd4cdab9df6af20656dd80ca955
woles109 <piotrek355@gmail.com>
2017-09-17 14:25:15 +0100

    Interfejs graficzny aplikacji

--- Mytravels/Mytravels/www/scripts/index.js
+++ Mytravels/Mytravels/www/scripts/index.js
@@ -1,23 +1,13 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
-// and then run "window.location.reload()" in the JavaScript Console.
-(function () {
+﻿(function () {
     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    document.addEventListener( 'deviceready', onDeviceReady, false );
 
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
+        document.addEventListener( 'pause', onPause, false );
+        document.addEventListener( 'resume', onResume, false );
         
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
     };
 
     function onPause() {
@@ -27,4 +17,5 @@
     function onResume() {
         // TODO: This application has been reactivated. Restore application state here.
     };
+
 } )();
