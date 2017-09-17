b43c40eb6e3432e1d497a0394c22591e20d1a919
woles109 <piotrek355@gmail.com>
2017-09-17 14:53:45 +0100

    Dodano pluginy

--- Mytravels/Mytravels/www/scripts/index.js
+++ Mytravels/Mytravels/www/scripts/index.js
@@ -1,6 +1,10 @@
 ﻿(function () {
     "use strict";
 
+    function $(id) {
+        return document.getElementById(id);
+    }
+
     document.addEventListener( 'deviceready', onDeviceReady, false );
 
     function onDeviceReady() {
@@ -8,6 +12,7 @@
         document.addEventListener( 'pause', onPause, false );
         document.addEventListener( 'resume', onResume, false );
         
+       
     };
 
     function onPause() {
